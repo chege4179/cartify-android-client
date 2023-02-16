0549c13ff5775bf07adc002fc2ed43cb4eb920f5
Peter Chege <peterkagure@gmail.com>
2023-02-16 05:02:22 +0300

    Added stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,28 +13,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import org.jetbrains.kotlin.builtins.StandardNames.FqNames.target
-import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
 
 buildscript {
     val compose_version = "1.4.2"
-    val kotlin_version  = "1.8.10"
+    val kotlin_version = "1.8.10"
     dependencies {
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
-        classpath ("com.google.gms:google-services:4.3.15")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath ("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.2.0")
     }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+} // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("com.android.application") version "7.4.1" apply false
-    id ("com.android.library") version "7.4.1" apply false
-    id ("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.diffplug.spotless") version "5.3.0"
     kotlin("plugin.serialization") version "1.8.10"
-
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+ktlint {
+    android.set(true)
+    verbose.set(true)
+    filter {
+        exclude { element -> element.file.path.contains("generated/") }
+    }
+}
+
+apply(plugin = "io.gitlab.arturbosch.detekt")
+detekt {
+    config = files("${project.rootDir}/detekt.yml")
+    parallel = true
+}
 apply(plugin = "com.diffplug.spotless")
 spotless {
     kotlin {
