44923ddb5f361afd2f32f8fa19a0b89a5173d3ad
Peter Chege <peterkagure@gmail.com>
2023-04-07 18:10:37 +0300

    Upgrade AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,15 +73,15 @@ android {
 dependencies {
 
     implementation( "androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.compose.ui:ui:1.4.0-beta01")
-    implementation ("androidx.compose.material:material:1.4.0-beta01")
+    implementation ("androidx.compose.ui:ui:1.5.0-alpha01")
+    implementation ("androidx.compose.material:material:1.5.0-alpha01")
     implementation ("androidx.compose.ui:ui-tooling-preview:1.4.0-beta01")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation ("androidx.activity:activity-compose:1.6.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation ("androidx.activity:activity-compose:1.7.0")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.3.3")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.4.0")
     debugImplementation ("androidx.compose.ui:ui-tooling:1.3.3")
 
 
@@ -92,17 +92,17 @@ dependencies {
     implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 
     // view model
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-beta01")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
 
     //coroutines
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     // dagger hilt
-    implementation ("com.google.dagger:hilt-android:2.44.2")
-    kapt ("com.google.dagger:hilt-android-compiler:2.44.2")
+    implementation ("com.google.dagger:hilt-android:2.45")
+    kapt ("com.google.dagger:hilt-android-compiler:2.45")
 //    implementation "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
     kapt ("androidx.hilt:hilt-compiler:1.0.0")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
@@ -110,17 +110,17 @@ dependencies {
     implementation ("io.coil-kt:coil-compose:2.2.2")
 
     //fcm
-    implementation ("com.google.firebase:firebase-messaging:23.1.1")
+    implementation ("com.google.firebase:firebase-messaging:23.1.2")
 
     // room
-    implementation ("androidx.room:room-runtime:2.5.0")
-    kapt ("androidx.room:room-compiler:2.5.0")
+    implementation ("androidx.room:room-runtime:2.5.1")
+    kapt ("androidx.room:room-compiler:2.5.1")
 
     // Kotlin Extensions and Coroutines support for Room
-    implementation ("androidx.room:room-ktx:2.5.0")
+    implementation ("androidx.room:room-ktx:2.5.1")
 
     // compose icons
-    implementation ("androidx.compose.material:material-icons-extended:1.3.1")
+    implementation ("androidx.compose.material:material-icons-extended:1.4.0")
     // glide
     implementation ("dev.chrisbanes.accompanist:accompanist-glide:0.5.1")
     //pager
@@ -129,7 +129,7 @@ dependencies {
     // swipe refresh
     implementation ("com.google.accompanist:accompanist-swiperefresh:0.27.1")
     // landscapist
-    implementation ("com.github.skydoves:landscapist-glide:1.4.8")
+    implementation ("com.github.skydoves:landscapist-glide:2.1.8")
 
     // datastore (core and preferences)
     implementation ("androidx.datastore:datastore:1.0.0")
@@ -148,7 +148,7 @@ dependencies {
     //paging 3
     implementation ("androidx.paging:paging-runtime-ktx:3.1.1")
     implementation ("androidx.paging:paging-compose:1.0.0-alpha18")
-    implementation ("androidx.profileinstaller:profileinstaller:1.3.0-beta01")
+    implementation ("androidx.profileinstaller:profileinstaller:1.3.0")
 
     //chucker -for http logging
     debugImplementation ("com.github.chuckerteam.chucker:library:3.5.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
     val compose_version = "1.4.2"
     val kotlin_version = "1.8.10"
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
@@ -26,14 +26,14 @@ buildscript {
     }
 } // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.1" apply false
-    id("com.android.library") version "7.4.1" apply false
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.diffplug.spotless") version "5.3.0"
     kotlin("plugin.serialization") version "1.8.10"
     id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
-    id("com.android.test") version "7.4.1" apply false
+    id("com.android.test") version "7.4.2" apply false
 }
 
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
